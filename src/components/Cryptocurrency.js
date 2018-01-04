91c73af67c11d1d367759e488e484a9dccbd850c
Katherine <katherine.a.kato@gmail.com>
2018-01-03 20:16:22 -0800

    Edited spacing

--- src/components/Cryptocurrency.js
+++ src/components/Cryptocurrency.js
@@ -14,16 +14,16 @@ class Cryptocurrency extends Component {
 
     return (
       <div className={'cryptocurrency ' + id}>
-      <div className="coin">
-        <div className="coin-left">
-          <img src={'http://katherinekato.com/misc/svg/' + symbol + '.svg'} alt={symbol} />
+        <div className="coin">
+          <div className="coin-left">
+            <img src={'http://katherinekato.com/misc/svg/' + symbol + '.svg'} alt={symbol} />
+          </div>
+          <div className="coin-content">
+            <h3 className="cryptocurrency-name">{name} ({symbol})</h3>
+            <h1 className="cryptocurrency-price"><NumberFormat value={price_usd} displayType={'text'} decimalScale={2} fixedDecimalScale={true} thousandSeparator={true} prefix={'$'} /></h1>
+            <h3 className={percent_change_1h > 0 ? 'percentage-increase' : 'percentage-decrease'}>{percent_change_1h}%</h3>
+          </div>
         </div>
-        <div className="coin-content">
-          <h3 className="cryptocurrency-name">{name} ({symbol})</h3>
-          <h1 className="cryptocurrency-price"><NumberFormat value={price_usd} displayType={'text'} decimalScale={2} fixedDecimalScale={true} thousandSeparator={true} prefix={'$'} /></h1>
-          <h3 className={percent_change_1h > 0 ? 'percentage-increase' : 'percentage-decrease'}>{percent_change_1h}%</h3>
-        </div>
-      </div>
       </div>
     );
   }
